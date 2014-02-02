aa65a83d5df3f147b699d1b47c3e10a984a210c7
Maximilien Paitel <max@z4n.fr>
2014-02-02 23:08:33 +0100

    Ajout d'un fichier de ping pour vérifier si l'application est en ligne. Evite d'avoir à rafraichir manuellement pour que le mode hors-ligne fonctionne.
    Signed-off-by: Maximilien Paitel <max@z4n.fr>

--- js/online.js
+++ js/online.js
@@ -1,2 +1,18 @@
-window.online = true;
-document.write('<script type="text/javascript" src="//www.google.com/jsapi?key=AIzaSyA5m1Nc8ws2BbmPRwKu5gFradvD_hgq6G0"></script>');
+// On vérifie via un ping si on est bien en ligne
+$.ajax({
+    cache: false,
+    async: false,
+    dataType: "json",
+    error: function () {
+        console.log('Erreur de connection.');
+        localStorage.status = 'offline';
+    },
+    success: function () {
+        console.log('Application connectée.');
+        document.write('<script type="text/javascript" src="//www.google.com/jsapi?key=AIzaSyA5m1Nc8ws2BbmPRwKu5gFradvD_hgq6G0"></script>');
+        localStorage.status = 'online';
+    },
+    timeout: 1000,
+    type: "GET",
+    url: "js/ping.js"
+});
